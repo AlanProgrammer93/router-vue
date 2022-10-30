048471a6c421206fc53280c74b769e4177421f8f
AlanExcelenciaDigital <azurita@excelenciadigital.net>
2022-10-29 22:12:03 -0300

    primer commit

---
+++ src/views/About.vue
@@ -0,0 +1,14 @@
+
+<template>
+	<main>
+		<img src="https://avatars.githubusercontent.com/u/76537764?v=4" alt="" />
+		<h1>About</h1>
+		<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem perferendis mollitia facere repellat ipsa. Quis sapiente ratione deserunt dolorem atque excepturi, sunt fuga voluptates est mollitia quisquam, explicabo impedit esse.</p>
+
+		<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem perferendis mollitia facere repellat ipsa. Quis sapiente ratione deserunt dolorem atque excepturi, sunt fuga voluptates est mollitia quisquam, explicabo impedit esse.
+			Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem perferendis mollitia facere repellat ipsa. Quis sapiente ratione deserunt dolorem atque excepturi, sunt fuga voluptates est mollitia quisquam, explicabo impedit esse.
+		</p>
+
+	</main>
+</template>
+
---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+
+<template>
+	<main>
+		<h1>Home</h1>
+		<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem perferendis mollitia facere repellat ipsa. Quis sapiente ratione deserunt dolorem atque excepturi, sunt fuga voluptates est mollitia quisquam, explicabo impedit esse.</p>
+
+		<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem perferendis mollitia facere repellat ipsa. Quis sapiente ratione deserunt dolorem atque excepturi, sunt fuga voluptates est mollitia quisquam, explicabo impedit esse.
+			Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem perferendis mollitia facere repellat ipsa. Quis sapiente ratione deserunt dolorem atque excepturi, sunt fuga voluptates est mollitia quisquam, explicabo impedit esse.
+		</p>
+
+	</main>
+</template>
+
